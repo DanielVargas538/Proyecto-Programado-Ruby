44f2e0ea5761aa238875faeac467ae3f428eb7d7
KevinJAM2314 <kevin.arroyomora@ucr.ac.cr>
2023-06-29 15:14:12 -0600

    Kevin Error action cable

---
+++ app/channels/order_channel.rb
@@ -0,0 +1,17 @@
+class OrderChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "OrderChannel"
+    send_order_data_to_channel
+  end
+
+  def unsubscribed
+    
+  end
+
+  private
+
+  def send_order_data_to_channel
+    order_data = Api::OrdersController.new.order_filtred
+    ActionCable.server.broadcast("OrderChannel", { order_data: order_data })
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -17,14 +17,13 @@ Rails.application.routes.draw do
   resources :manage_dishes
   resources :manage_orders
   resources :manage_clients, only: [:index, :update]
-
+  mount ActionCable.server => '/cable'
   scope module: :api do
     resources :user_logs
     get '/user_logs/params/:email/:password', to: 'user_logs#verify_params', email: /[^\/]+/
 
     get '/orders/filter', to: 'orders#order_filtred'
     resources :orders
-   # mount ActionCable.server => '/cable'
   end
 
   devise_for :users , path_names: { sign_in: 'login', 
